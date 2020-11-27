b38cb9ffefd0069b63f386cf27afb7accd7fb4b2
rahaf83 <rahaf.migdad@hotmail.com>
2020-11-27 13:58:48 +0200

    v4

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -29,7 +29,7 @@ var itemSchema = mongoose.Schema({
       }
   });
   //Create Model
-  module.exports=mongoose.model("Item",itemSchema)
+module.exports=mongoose.model("Item",itemSchema)
 
 
 var selectAll = function(callback) {
@@ -41,6 +41,7 @@ var selectAll = function(callback) {
     }
   });
 };
+//hey
 
 function save(data){
   var itemform=new Item({
@@ -48,6 +49,7 @@ function save(data){
     description:data.description,
     img:data.img
   });
+
   itemform.save(function(err,doc){
     if(err) throw err;
     console.log("connected")
